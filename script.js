f22a17959bbc3e616f6d7b16e536f14d33c2b346
Jeff-Mwai <jeffmwai3@gmail.com>
2020-08-21 23:09:55 +0300

    Add two objects to the file

---
+++ script.js
@@ -0,0 +1,24 @@
+let males = {
+    Sunday: "Kwasi",
+    Monday: "Kwandwo",
+    Tuesday: "Kwabena",
+    Wednesday: "Kwaku",
+    Thursday:"Yaw",
+    Friday: "Kofi",
+    Saturday: "Kwame",
+};
+let females = {
+    Sunday: "Akosua",
+    Monday: "Adwoa",
+    Tuesday: "Abenaa",
+    Wednesday: "Akua",
+    Thursday:"Afua",
+    Friday: "Kofi",
+    Saturday: "Ama",
+};
+
+let dateOfBirth = prompt("Enter date of birth: ");
+let CC = century;
+let YY = year;
+let MM = month;
+let DD = dayBorn;
