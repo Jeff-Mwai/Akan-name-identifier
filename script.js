8bcfd3b7da4be69686bff3ca04ee61d894177eff
Jeff-Mwai <jeffmwai3@gmail.com>
2020-08-22 00:42:48 +0300

    Add indexes that represent weekdays

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ let CC = century;
 let YY = year;
 let MM = month;
 let DD = dayBorn;
+
+var d = new date ();
+var weekday = new array (7);
+weekday [0] = "Sunday";
+weekday [1] = "Monday";
+weekday [2] = "Tuesday";
+weekday [3] = "Wednesday";
+weekday [4] = "Thursday";
+weekday [5] = "Friday";
+weekday [6] = "Saturday";
