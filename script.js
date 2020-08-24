413dce45f93dcaec44d1ba033aa887d4c9ae0e98
Jeff-Mwai <jeffmwai3@gmail.com>
2020-08-24 10:45:31 +0300

    Add function add rows

--- script.js
+++ script.js
@@ -69,6 +69,24 @@ function getName(){
         //clear inputs
          document.getElementById('gender').value = '';
          document.getElementById('dob').value = '';
+         addRows(gender, dob,name)
+
+        //Add rows to table
+    
 
     }
+}
+
+
+function addRows(x,y,z) {
+    var table = document.getElementById("table");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    cell1.innerHTML=x;
+    cell2.innerHTML=y;
+    cell3.innerHTML=z;
+
 }
